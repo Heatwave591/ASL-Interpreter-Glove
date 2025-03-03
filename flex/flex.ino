d68c44709fd91b16cb48e34221b7f6aedf84d5b6
Anirudhhan Raghuraman <85730720+Heatwave591@users.noreply.github.com>
2025-03-03 12:54:21 -0500

    create webserver_SPIFFS and flex

---
+++ flex/flex.ino
@@ -0,0 +1,58 @@
+// Om Namo Narayana
+
+const int F1 = A0; 
+const int F2 = A1;
+const int F3 = A2;
+const int F4 = A3;
+const int F5 = A4;
+
+int f_volt1;
+int f_volt2;
+int f_volt3;
+int f_volt4;
+int f_volt5;
+
+void setup(){
+  Serial.begin(115200);      
+}
+
+void loop(){
+  f_volt1 = analogRead(F1);
+  f_volt2 = analogRead(F2);
+  f_volt3 = analogRead(F3);
+  f_volt4 = analogRead(F4);
+  f_volt5 = analogRead(F5);
+
+  Serial.print("Finger 1: ");
+  Serial.print(f_volt1);
+  Serial.print("\t"); 
+
+  Serial.print("Finger 2:");
+  Serial.print(f_volt2);
+  Serial.print("\t");
+
+  Serial.print("Finger 3:");
+  Serial.print(f_volt3);
+  Serial.print("\t");
+
+  Serial.print("Finger 4:");
+  Serial.print(f_volt4);
+  Serial.print("\t");
+
+  Serial.print("Finger 5:");
+  Serial.print(f_volt5);
+  Serial.print("\t");
+
+  if (f_volt1 > 200 && f_volt2 < 2000){
+      Serial.println("Water");
+    }
+
+  else if (f_volt1 <200 && f_volt2 > 2000){
+    Serial.println("Food");
+  }
+  
+  else{
+      Serial.println("3, 4, 5");
+    }
+    delay(2500);                   
+  }
