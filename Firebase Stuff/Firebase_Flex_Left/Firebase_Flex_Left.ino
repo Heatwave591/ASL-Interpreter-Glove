13ccd979b22d227d95a60e76970553202929d9a9
Anirudhhan Raghuraman <85730720+Heatwave591@users.noreply.github.com>
2025-04-08 22:06:55 -0400

    Added inferences for left hand

--- Firebase Stuff/Firebase_Flex_Left/Firebase_Flex_Left.ino
+++ Firebase Stuff/Firebase_Flex_Left/Firebase_Flex_Left.ino
@@ -41,8 +41,58 @@ FirebaseConfig config;
 
 String uid;
 String databasePath;
+String predictedLetter = "-";
 
 unsigned long sendDataPrevMillis = 0;
+
+// === Character Range Detection Struct ===
+struct FingerRange {
+  int minVal[5];
+  int maxVal[5];
+  char letter;
+};
+
+FingerRange letterModels[] = {
+  {{1554, 2017, 1293, 1067, 0}, {2906, 3042, 2415, 2295, 0}, 'a'},
+  {{0, 0, 0, 0, 774}, {0, 199, 0, 0, 1252}, 'b'},
+  {{76, 717, 123, 198, 0}, {1424, 1885, 1185, 995, 0}, 'c'},
+  {{346, 1521, 640, 0, 0}, {1854, 2527, 1671, 0, 0}, 'd'},
+  {{453, 1680, 1111, 859, 380}, {1967, 2470, 1671, 1855, 1828}, 'e'},
+  {{0, 0, 0, 205, 0}, {0, 0, 0, 991, 0}, 'f'},
+  {{727, 1771, 1035, 0, 0}, {2000, 2467, 1899, 0, 0}, 'g'},
+  {{1059, 1023, 0, 0, 0}, {2303, 1924, 0, 0, 0}, 'h'},
+  {{0, 377, 402, 169, 117}, {0, 2615, 2037, 2309, 1238}, 'i'},
+  {{29, 308, 0, 0, 0}, {1870, 1136, 53, 0, 0}, 'k'},
+  {{111, 1554, 903, 0, 17}, {1421, 2294, 1719, 0, 987}, 'l'},
+  {{214, 1296, 244, 51, 2}, {2183, 2339, 1392, 1279, 1150}, 'm'},
+  {{64, 228, 138, 146, 39}, {2093, 2493, 1408, 1117, 1488}, 'n'},
+  {{23, 441, 194, 13, 0}, {1447, 2023, 1528, 952, 163}, 'o'},
+  {{91, 32, 0, 0, 0}, {1492, 1657, 0, 0, 0}, 'p'},
+  {{221, 1793, 586, 0, 0}, {1761, 2533, 1900, 0, 6}, 'q'},
+  {{159, 772, 0, 64, 0}, {2154, 2163, 1143, 1078, 626}, 'r'},
+  {{71, 1424, 43, 400, 6}, {2377, 2293, 1792, 1233, 1251}, 's'},
+  {{113, 305, 343, 31, 54}, {1794, 1385, 1600, 894, 1387}, 't'},
+  {{33, 291, 0, 0, 0}, {2076, 1774, 0, 0, 0}, 'u'},
+  {{48, 752, 0, 0, 0}, {2134, 2134, 0, 0, 434}, 'v'},
+  {{90, 0, 0, 0, 0}, {1844, 0, 0, 0, 780}, 'w'},
+  {{31, 941, 133, 0, 158}, {1614, 2559, 1607, 961, 1271}, 'x'},
+  {{0, 63, 13, 45, 0}, {16, 1961, 1250, 677, 0}, 'y'},
+  {{0, 1006, 922, 0, 0}, {0, 1726, 1632, 0, 0}, 'ILY'}
+};
+const int modelCount = sizeof(letterModels) / sizeof(letterModels[0]);
+
+char inferLetterFromRange(int pinky, int ring, int middle, int index, int thumb) {
+  for (int i = 0; i < modelCount; i++) {
+    bool match = true;
+    if (pinky  < letterModels[i].minVal[0] || pinky  > letterModels[i].maxVal[0]) match = false;
+    if (ring   < letterModels[i].minVal[1] || ring   > letterModels[i].maxVal[1]) match = false;
+    if (middle < letterModels[i].minVal[2] || middle > letterModels[i].maxVal[2]) match = false;
+    if (index  < letterModels[i].minVal[3] || index  > letterModels[i].maxVal[3]) match = false;
+    if (thumb  < letterModels[i].minVal[4] || thumb  > letterModels[i].maxVal[4]) match = false;
+    if (match) return letterModels[i].letter;
+  }
+  return '-';
+}
 
 void setup() {
   Serial.begin(115200);
@@ -193,7 +243,14 @@ void loop() {
     Serial.print("Finger 5: ");
     Serial.print(f_volt5);
     Serial.println();
-    
+
+    // Predict character based on finger range logic
+    char predicted = inferLetterFromRange(f_volt1, f_volt2, f_volt3, f_volt4, f_volt5);
+    predictedLetter = String(predicted);
+
+    Serial.print("Predicted letter: ");
+    Serial.println(predicted);
+
     f_volt1 = random(1024);
     f_volt2 = random(1024);
     f_volt3 = random(1024);
@@ -216,6 +273,7 @@ void loop() {
     json.set("Gyroscope Z", gyroZ);
     json.set("Net Acceleration", accNet);
     json.set("Tempreture", tempr);
+    json.set("Predicted Letter", predictedLetter);
 
     //Testing code ends here
 
